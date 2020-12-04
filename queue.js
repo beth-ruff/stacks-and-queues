149c161819baae102d9dc4d9bb4de3b49a589ef5
Beth Ruff <beth.ruff2@gmail.com>
2020-12-04 15:03:55 -0500

    add enqueue and dequeue methods

--- queue.js
+++ queue.js
@@ -1,3 +1,6 @@
+//enqueue - add to end (push)
+//dequeue - remove from beginning (shift)
+
 class Node {
     constructor(value){
         this.value = value;
@@ -10,5 +13,28 @@ class Queue {
         this.first = null;
         this.last = null;
         this.size = 0;
+    }
+
+    enqueue(val) {
+        let newNode = new Node(val);
+        if(!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        return ++this.size;
+    }
+
+    dequeue() {
+        if(!this.first) return null;
+        let temp = this.first;
+        if(this.first === this.last){
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.value;
     }
 }
